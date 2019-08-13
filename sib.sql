eda61ba2009ac40d6182453f61602c46882d6c7f
Nurjaman <nurzaman02@gmail.com>
2019-08-13 07:15:46 +0700

    Sistem Informasi Billboard 1308

--- sib.sql
+++ sib.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2019 at 09:43 PM
+-- Generation Time: Aug 13, 2019 at 02:13 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.1.28
 
@@ -105,8 +105,7 @@ INSERT INTO `reklame` (`ID`, `id_user`, `name`, `price`, `latitude`, `longitude`
 (5, 0, 'CEK KE LIMA 5', 75000000, '-6.877697544171006', '107.54149816020504', 'ALAMAT BLA BLA BLA', 'bikin-ngiler-ini-gaji-magang-di-perusahaan-teknologi-raksasa.png', 'Full Service, Pantau CCTV, Lokasi Ramai', 'DESKRIPSI BLA BLA', 'Billboard', 'Vertical', '4 x 10 Meter', 'Frontlight', 'Stand Alone', 'http://facebook.com/dmons02', 'Tidak Tersedia'),
 (6, 8, 'PT. Reklame Iklan', 75000000, '-6.873948155025596', '107.51523396953121', 'Alamat lengkap woow', 'Screenshot_4.png', 'Full Service', 'Deskripsi', 'Billboard', 'Vertical', '4 x 10 Meter', 'Frontlight', 'Bridge', 'http://facebook.com/dmons02', 'Tersedia'),
 (7, 7, 'CEK KE LIMA 6', 5000000, '-6.869176162427446', '107.50768086894527', 'adsadsada', 'SIB.png', 'Full Service, Pantau CCTV', 'wqeqwewqeweqweqw', 'Neon Sign', 'Vertical', '4 x 10 Meter', 'Frontlight', 'Bridge', 'http://facebook.com/dmons02', 'Tersedia'),
-(8, 7, 'Coba', 100000, '-6.8736073', '107.53772212578122', 'Cimahi', 'billboard13.png', 'Full Service', 'Tes', 'Billboard', 'Horizontal', '4 x 10 Meter', 'Bridge', 'LED', 'http://facebook.com/dmons02', 'Tersedia'),
-(9, 8, 'cecep', 500000000000000000, '-6.9536073', '107.5566327', 'uciedb', 'Screenshot_41.png', 'Full Service', 'cjbk', 'Neon Sign', 'Vertical', '4 x 10 Meter', 'Stand Alone', 'Frontlight', 'jbnj', 'Tersedia');
+(8, 7, 'Coba', 100000, '-6.8736073', '107.53772212578122', 'Cimahi', 'billboard13.png', 'Full Service', 'Tes', 'Billboard', 'Horizontal', '4 x 10 Meter', 'Bridge', 'LED', 'http://facebook.com/dmons02', 'Tersedia');
 
 -- --------------------------------------------------------
 
@@ -144,7 +143,7 @@ CREATE TABLE `tbl_order` (
   `no_invoice` varchar(15) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_reklame` int(11) NOT NULL,
-  `id_perusahaan` int(11) NOT NULL,
+  `perusahaan_id` int(11) NOT NULL,
   `description` text NOT NULL,
   `photo_order` varchar(250) NOT NULL,
   `status_order` varchar(2) NOT NULL,
@@ -156,18 +155,19 @@ CREATE TABLE `tbl_order` (
 -- Dumping data for table `tbl_order`
 --
 
-INSERT INTO `tbl_order` (`no_invoice`, `id_user`, `id_reklame`, `id_perusahaan`, `description`, `photo_order`, `status_order`, `tgl_acc`, `tanggal`) VALUES
-('0607190004', 8, 1, 0, 'vik', 'Screenshot_43.png', '1', '', '2019-07-19 07:54:29'),
-('1406190001', 2, 5, 0, 'Mantuls pokokkna', 'visi_dan_misi.jpg', '1', '', '2019-06-21 20:51:10'),
-('1507190001', 1, 1, 0, 'adsadsada', 'billboard2.png', '1', '', '2019-07-19 07:54:33'),
-('2207190001', 6, 1, 0, 'Cek yang sudah memiliki id perusahaan', 'Cimindi.png', '0', '', '2019-07-22 10:09:22'),
-('2207190002', 6, 1, 0, 'okokok', '', '0', '', '2019-07-22 11:59:23'),
-('2207190003', 6, 1, 0, 'dasdsadsa', 'Screenshot_44.png', '0', '', '2019-07-22 12:08:35'),
-('2207190004', 6, 1, 4, 'dasdasd', 'billboard21.png', '0', '', '2019-07-22 12:11:27'),
-('2207190005', 6, 1, 4, 'dasdasdsad', 'b143_4-512.png', '0', '', '2019-07-22 12:12:10'),
-('2207190006', 11, 1, 6, 'PT Nurjaman', 'billboard22.png', '0', '', '2019-07-22 16:33:35'),
-('2406190001', 2, 1, 0, 'saya akan iklan tentang jarum black', 'Screenshot_15.png', '1', '', '2019-07-19 07:54:35'),
-('3105190002', 2, 1, 0, 'Bangkong', 'Virtual-internship-1024x682.png', '1', '', '2019-07-19 07:54:38');
+INSERT INTO `tbl_order` (`no_invoice`, `id_user`, `id_reklame`, `perusahaan_id`, `description`, `photo_order`, `status_order`, `tgl_acc`, `tanggal`) VALUES
+('0308190001', 6, 1, 5, 'dsadasd', 'billboard11.png', '0', '', '2019-08-03 16:51:04'),
+('0607190004', 8, 1, 6, 'vik', 'Screenshot_43.png', '0', '', '2019-07-31 09:01:32'),
+('1406190001', 2, 5, 6, 'Mantuls pokokkna', 'visi_dan_misi.jpg', '1', '', '2019-07-31 09:01:43'),
+('1507190001', 1, 1, 6, 'adsadsada', 'billboard2.png', '1', '', '2019-07-24 17:13:54'),
+('2207190001', 6, 1, 6, 'Cek yang sudah memiliki id perusahaan', 'Cimindi.png', '1', '', '2019-07-29 17:47:58'),
+('2207190002', 6, 1, 6, 'okokok', '', '1', '', '2019-04-29 17:48:06'),
+('2207190003', 6, 1, 6, 'dasdsadsa', 'Screenshot_44.png', '1', '', '2019-05-29 17:48:04'),
+('2207190004', 6, 1, 4, 'dasdasd', 'billboard21.png', '1', '', '2019-06-29 17:48:00'),
+('2207190005', 6, 1, 4, 'dasdasdsad', 'b143_4-512.png', '1', '', '2019-05-29 17:48:03'),
+('2207190006', 11, 1, 6, 'PT Nurjaman', 'billboard22.png', '1', '', '2019-03-29 17:48:09'),
+('2406190001', 2, 1, 6, 'saya akan iklan tentang jarum black', 'Screenshot_15.png', '1', '', '2019-02-24 17:13:38'),
+('3105190002', 2, 1, 6, 'Bangkong', 'Virtual-internship-1024x682.png', '1', '', '2019-07-24 17:13:51');
 
 -- --------------------------------------------------------
 
@@ -176,8 +176,8 @@ INSERT INTO `tbl_order` (`no_invoice`, `id_user`, `id_reklame`, `id_perusahaan`,
 --
 
 CREATE TABLE `tbl_perusahaan` (
-  `id_perusahaan` int(50) NOT NULL,
-  `id_user` int(50) NOT NULL,
+  `id_perusahaan` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `nm_perusahaan` varchar(50) NOT NULL,
   `jabatan_perusahaan` varchar(50) NOT NULL,
   `direktur_perusahaan` varchar(50) NOT NULL,
@@ -196,7 +196,7 @@ CREATE TABLE `tbl_perusahaan` (
 INSERT INTO `tbl_perusahaan` (`id_perusahaan`, `id_user`, `nm_perusahaan`, `jabatan_perusahaan`, `direktur_perusahaan`, `kontak_perusahaan`, `mobile_perusahaan`, `fax_perusahaan`, `alamat_perusahaan`, `created_at`, `updated_at`) VALUES
 (1, 8, 'Tes', 'dasdas', 'asdsad', 'dasd', 'asd', 'asda', 'dasda', '2019-07-02 17:57:58', '0000-00-00 00:00:00'),
 (4, 6, 'doqwkdpoqkod', 'kpokdpokwpokdw', 'opkdp', 'wok', 'wdkp', 'dwkp', 'wkdpo', '2019-07-21 21:45:48', '0000-00-00 00:00:00'),
-(5, 6, 'PT. Billboard Utama', 'Direktu Utama', 'Nurjaman', 'Usep Uhuy', '62089672255644', '40513', 'Alamaatnya ada di cimahi', '2019-07-21 21:46:30', '0000-00-00 00:00:00'),
+(5, 6, 'PT. Nurjaman', 'Jabatan Nurjaman', 'Nama Direktur d', 'Dadang Sutisna', '022-89672255', '(0516) 21510', 'Alamatnya lengkap weh', '2019-07-21 21:46:30', '0000-00-00 00:00:00'),
 (6, 11, 'PT. Nurjaman', 'Jabatan Nurjaman', 'Nama Direktur', 'Dadang Sutisna', '022-89672255', '(0516) 21510', 'Alamatnya lengkap weh', '2019-07-22 16:33:07', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
@@ -250,17 +250,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userId`, `fullname`, `kota`, `kode_pos`, `alamat`, `mobile`, `username`, `email`, `password`, `reset_password`, `status_aktif`, `role`, `photo_profile`, `photo_npwp`, `photo_sppkp`, `photo_siup`, `created_at`, `updated_at`) VALUES
-(1, 'ini pertama', 'Coba', 'dasdasd', 'asdasda', 'asdasda', 'asdqwqw', '', '$2y$10$SAvFim22ptA9gHVORtIaru1dn9rhgerJlJCPxRNA02MjQaJnkxawq', '', 0, 'Admin', NULL, '', '', '', '2019-07-01 20:12:43', NULL),
-(2, NULL, NULL, '', '', NULL, '', '', '$2y$10$JUaLtr5STNqnwXRg7wd3O.Xx6sWf.J2HiGFTFx/923VpT1s9atK8y', '', 0, 'Admin', NULL, '', '', '', '2019-07-01 20:12:43', NULL),
 (3, 'Tes Penyewa', '', '', '', '0852159185', 'tester', 'testes@gmail.com', '$2y$10$JUaLtr5STNqnwXRg7wd3O.Xx6sWf.J2HiGFTFx/923VpT1s9atK8y', 'sz7TIv4xYR3iDQWgoXFwu0EV8NKtSBMclfUqb2mjyhHnG6LPA5', 1, 'Penyewa', NULL, '', '', '', '2019-07-01 20:12:43', NULL),
-(4, NULL, '', '', '', 'bisadong', 'bismillah', 'admin1@admin.com', 'nurjaman', '', 1, 'Penyewa', NULL, '', '', '', '2019-07-01 20:12:43', NULL),
-(5, 'Tes Pemilik Media', '', '', '', '0893123213', 'Nurjaman', 'nurjaman@admin.com', '$2y$10$6/iaK7PS7X9xG.Jf1LB//est0/cu5frKcRafcqHymvot7R9yxexOC', '', 1, 'Pemilik Media', NULL, '', '', '', '2019-07-01 20:12:43', NULL),
+(5, 'Tes Pemilik Media', '', '', '', '0893123213', 'Nurjaman', 'nurjaman@admin.com', '$2y$10$6/iaK7PS7X9xG.Jf1LB//est0/cu5frKcRafcqHymvot7R9yxexOC', '', 1, 'Pemilik Media', 'cimahi-slide-1.png', '', '', '', '2019-07-01 20:12:43', NULL),
 (6, 'Penyewa', '', '', '', '089672255644', 'Penyewa', 'penyewa@sib.com', '$2y$10$IFmdxn9P7QUeRxy3OGetvugXYmi40K8scXSkQZz5Ogph0Ki5/9oDW', '', 1, 'Penyewa', 'visi_dan_misi6.jpg', '', '', '', '2019-07-01 20:12:43', NULL),
 (7, 'Pemilik', '', '', '', '089672255644', 'Pemilik Media', 'pemilik@sib.com', '$2y$10$Lf6IUE9XTc7zoZZZnwDog.XVHUwRGOeTMnM89uMWR8SIvHinMB/qe', '', 1, 'Pemilik Media', 'visi_dan_misi7.jpg', 'bikin-ngiler-ini-gaji-magang-di-perusahaan-teknologi-raksasa1.png', 'Cimindi.png', '', '2019-07-01 20:12:43', NULL),
-(8, 'Admin Coba 12356', 'Cimahi 123', '40513', 'Test Admin                                                                               ', '089672255644', 'admin02', 'admin@sib.com', '$2y$10$Lf6IUE9XTc7zoZZZnwDog.XVHUwRGOeTMnM89uMWR8SIvHinMB/qe', '', 1, 'Admin', 'billTag.png', NULL, NULL, NULL, '2019-07-01 20:12:43', NULL),
-(9, 'Syam Ardy', 'uyuyu', '', '                    ', '0896723173615', 'Syam', 'syam@sib.com', '$2y$10$WyzToYpuHgvxn.v3V6cD.OzC3y0698CRZRU94CijoMymEx7LLUeOq', '', 1, 'Admin', 'bikin-ngiler-ini-gaji-magang-di-perusahaan-teknologi-raksasa.png', 'Cimindi6.png', 'program-1-magang1.png', 'Virtual-internship-1024x6829.png', '2019-07-10 11:24:36', NULL),
-(10, NULL, NULL, '', '', NULL, 'das', 'nurjaman@sib.com', '$2y$10$ncH3KPwoXINEugi7y6zVCuCfkoobGz3Mj2ryv0ZfKrWWxjuDMrXQa', '', 1, 'Admin', NULL, NULL, NULL, NULL, '2019-07-15 19:35:34', NULL),
-(11, 'Admin Coba', NULL, '', '', '628957129381', 'tes', 'nurzaman02@gmail.com', '$2y$10$I2PxKtLVYH/eu49bwj9wMeSn.bABjhsstZd6erMEyRwW3MHihu6RS', 'H8MdEgm7XtczKuk5TDOCUpJYRFrbsAexZ2G1VPvoSQNlnWf460', 1, 'Penyewa', NULL, NULL, NULL, NULL, '2019-07-15 19:38:18', NULL);
+(8, 'Admin Coba 12356', 'Cimahi 12345', '40513', '    Test Admin                                                                                   ', '089672255644', 'admin02', 'admin@sib.com', '$2y$10$Lf6IUE9XTc7zoZZZnwDog.XVHUwRGOeTMnM89uMWR8SIvHinMB/qe', '', 1, 'Admin', 'billTag.png', 'billboard2.png', 'cimahi-slide-1.png', NULL, '2019-07-01 20:12:43', NULL),
+(9, 'Syam Ardy', 'uyuyua123123', 'dsadsa', '  dasdas  ', '0896723173615dasda', 'Syam', 'syam@sib.com', '$2y$10$WyzToYpuHgvxn.v3V6cD.OzC3y0698CRZRU94CijoMymEx7LLUeOq', '', 1, 'Penyewa', 'bikin-ngiler-ini-gaji-magang-di-perusahaan-teknologi-raksasa.png', 'Cimindi6.png', 'program-1-magang1.png', 'Virtual-internship-1024x6829.png', '2019-07-10 11:24:36', NULL),
+(10, 'Nurjaman', '', '', '  ', '', 'das', 'nurjaman@sib.com', '$2y$10$ncH3KPwoXINEugi7y6zVCuCfkoobGz3Mj2ryv0ZfKrWWxjuDMrXQa', '', 1, 'Admin', NULL, NULL, NULL, NULL, '2019-07-15 19:35:34', NULL),
+(11, 'Admin Coba', NULL, '', '', '628957129381', 'tes', 'nurzaman02@gmail.com', '$2y$10$I2PxKtLVYH/eu49bwj9wMeSn.bABjhsstZd6erMEyRwW3MHihu6RS', 'JT7ovzPeAcL56Ojrf03i1ME94lUnSCtdGDyFwka2gZxKXVbuNh', 1, 'Penyewa', NULL, NULL, NULL, NULL, '2019-07-15 19:38:18', NULL),
+(12, NULL, NULL, '', '', '089672255644', 'coba', 'coba23@sib.com', '$2y$10$dotQR7vf9FEZNFtpGAGNreoZAPdFVDzIb0ro6Up6A1Mxm6q7Ctkj.', '', 1, 'Penyewa', NULL, NULL, NULL, NULL, '2019-08-01 20:26:43', NULL),
+(13, 'bambang 23', '', '', '    ', '089672255644', 'bambang02', 'bambang@sib.com', '$2y$10$rB7lZ1wmWcQh8nz4pCib7.iMZ2KaPNyhy8cOeoMGUUNwItCX1.R8S', '', 1, 'Penyewa', NULL, NULL, NULL, NULL, '2019-08-12 10:19:49', NULL);
 
 --
 -- Indexes for dumped tables
@@ -294,7 +293,7 @@ ALTER TABLE `reklamecategories`
 -- Indexes for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  ADD PRIMARY KEY (`no_invoice`,`id_user`,`id_reklame`,`id_perusahaan`);
+  ADD PRIMARY KEY (`no_invoice`,`id_user`,`id_reklame`,`perusahaan_id`);
 
 --
 -- Indexes for table `tbl_perusahaan`
@@ -346,7 +345,7 @@ ALTER TABLE `reklamecategories`
 -- AUTO_INCREMENT for table `tbl_perusahaan`
 --
 ALTER TABLE `tbl_perusahaan`
-  MODIFY `id_perusahaan` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_perusahaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_reset_password`
@@ -358,7 +357,7 @@ ALTER TABLE `tbl_reset_password`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `userId` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
